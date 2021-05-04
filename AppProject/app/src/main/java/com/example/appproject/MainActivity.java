6f3230aa4fb210cd73f4d2d57d2175c04eadbe1f
c20emili <c20emili@student.his.se>
2021-05-04 09:54:38 +0200

    added a menu

--- AppProject/app/src/main/java/com/example/appproject/MainActivity.java
+++ AppProject/app/src/main/java/com/example/appproject/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.appproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menu_item_1) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
