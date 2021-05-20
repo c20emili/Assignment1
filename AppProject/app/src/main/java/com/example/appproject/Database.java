72b70bb6412004690d75eeeb6ffc0c723f653166
c20emili <c20emili@student.his.se>
2021-05-20 14:50:16 +0200

    Changed tables

--- AppProject/app/src/main/java/com/example/appproject/Database.java
+++ AppProject/app/src/main/java/com/example/appproject/Database.java
@@ -12,6 +12,8 @@ public class Database {
         static final String COLUMN_NAME_COST = "cost";
         static final String COLUMN_NAME_LOCATION = "location";
         static final String COLUMN_NAME_CATEGORY = "category";
+        static final String COLUMN_NAME_DEPTH = "depth";
+        static final String COLUMN_NAME_WIKI = "wiki";
     }
 
     public static final String SQL_CREATE_TABLE_LAKE =
@@ -22,28 +24,13 @@ public class Database {
                 Lake.COLUMN_NAME_SIZE + " INT," +
                 Lake.COLUMN_NAME_COST + " INT," +
                 Lake.COLUMN_NAME_LOCATION + " TEXT," +
-                Lake.COLUMN_NAME_CATEGORY + " TEXT) ";
+                Lake.COLUMN_NAME_CATEGORY + " TEXT," +
+                Lake.COLUMN_NAME_DEPTH + " INT," +
+                Lake.COLUMN_NAME_WIKI + " TEXT)" ;
 
     public static final String SQL_DELETE_TABLE_LAKE =
             "DROP TABLE IF EXISTS " + Lake.MAIN_TABLE_NAME;
 
-    static class Auxdata{
-        static final String AUX_TABLE_NAME = "auxdata";
-        static final String COLUMN_NAME_DEPTH = "depth";
-        static final String COLUMN_NAME_WIKI = "wiki";
-        static final String COLUMN_NAME_IMG = "img";
-    }
-
-    public static final String SQL_CREATE_TABLE_AUX =
-            "CREATE TABLE " + Auxdata.AUX_TABLE_NAME + " (" +
-                    Lake.COLUMN_NAME_ID + " INTEGER FOREIGN PRIMARY KEY," +
-                    Auxdata.COLUMN_NAME_DEPTH + " TEXT," +
-                    Auxdata.COLUMN_NAME_WIKI + " TEXT," +
-                    Auxdata.COLUMN_NAME_IMG + " TEXT" +
-                    Fish.COLUMN_NAME_LATIN + " INTEGER FOREIGN KEY)";
-
-    public static final String SQL_DELETE_TABLE_AUX =
-            "DROP TABLE IF EXISTS " + Auxdata.AUX_TABLE_NAME;
 
     static class Fish{
         static final String FISH_TABLE_NAME = "fish";
